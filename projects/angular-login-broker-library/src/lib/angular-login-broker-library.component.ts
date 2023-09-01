9d1ed9458356876a69120f74c4fd0854eea92542
fluttertalent <ritchiejosie157@gmail.com>
2023-09-02 02:20:28 +0900

    library update

--- projects/angular-login-broker-library/src/lib/angular-login-broker-library.component.ts
+++ projects/angular-login-broker-library/src/lib/angular-login-broker-library.component.ts
@@ -2,40 +2,29 @@ import { Component, Input } from '@angular/core';
 import { AngularLoginBrokerLibraryService } from './angular-login-broker-library.service';
 
 @Component({
-  selector: 'lib-angular-login-broker-library',
+  selector: 'app-login-broker-button',
   template: `
-    <button (click)="startLoginProcess()">Login with {{ platform }}</button>
+    <button class="login-broker-button login-broker-{{ platform }}-button" (click)="startLoginProcess(tenantName, platform)">
+      <i class="fab fa-{{ platform }}"></i>
+      Login with {{ platform }}
+    </button>
   `,
-  styles: [
-  ]
+  styleUrls: ['./angular-login-broker-library.css']
 })
 export class AngularLoginBrokerLibraryComponent {
-  @Input()
-  platform!: string;
-  @Input()
-  tenantName!: string;
-
+  @Input() tenantName!: string;
+  @Input() platform!: string;
+  @Input() onSessionReceived!: (sessionId: string) => void;
+  @Input() onErrorReceived!: (error: string) => void;
+  
   constructor(private loginBrokerService: AngularLoginBrokerLibraryService) {}
 
-  startLoginProcess() {
-    this.loginBrokerService
-      .startLoginProcess(this.tenantName, this.platform)
-      .subscribe(
-        (result) => {
-          if (result === 'completed') {
-            // Handle session completed
-          } else if (result === 'failed') {
-            console.log('Login failed. Try again');
-            // Handle login failure
-          } else {
-            console.log('Session not yet available');
-            // Handle other cases
-          }
-        },
-        (error) => {
-          console.error(error);
-          // Handle errors
-        }
-      );
+  public startLoginProcess(): void {
+    this.loginBrokerService.startLoginProcess(
+      this.tenantName,
+      this.platform,
+      this.onSessionReceived,
+      this.onErrorReceived
+    );
   }
 }
--- projects/angular-login-broker-library/src/lib/angular-login-broker-library.service.ts
+++ projects/angular-login-broker-library/src/lib/angular-login-broker-library.service.ts
@@ -1,19 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, timer } from 'rxjs';
-import { catchError, retryWhen, switchMap, take } from 'rxjs/operators';
+import { Observable, throwError, timer } from 'rxjs';
+import { catchError, switchMap, take, tap } from 'rxjs/operators';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AngularLoginBrokerLibraryService {
-  private MAX_RETRY_COUNT = 60; // Maximum retry count (2 minutes with 2-second intervals)
+  private MAX_RETRY_COUNT = 60;
+  private sessionId: string = '';
+  private retryCount = 0;
+  private hasBeenPending = false;
 
   constructor(private http: HttpClient) {}
 
   private generateRandomString(length: number): string {
-    const allowedChars =
-      'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    const allowedChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let randomString = '';
 
     for (let i = 0; i < length; i++) {
@@ -25,54 +27,77 @@ export class AngularLoginBrokerLibraryService {
     return randomString;
   }
 
-  private handleError(error: any): void {
-    console.error(error);
-    // Handle the error as needed
-  }
-
-  private fetchStatus(
-    tenantName: string,
-    currentSessionId: string
-  ): Observable<string> {
-    return this.http
-      .get(`https://api.login.broker/${tenantName}/auth/status/${currentSessionId}`, {
-        responseType: 'text',
-      })
-      .pipe(
-        catchError((error) => {
-          this.handleError(error);
-          return [];
+  private fetchStatus(currentSessionId: string, onErrorReceived: (error: string) => void, onSessionReceived: (sessionId: string) => void): Observable<string> {
+    console.log('fetchStatus starting');
+    console.log('currentSessionId:', currentSessionId);
+    if (currentSessionId) {
+      return this.http.get<string>(`https://api.login.broker/${tenantName}/auth/status/${currentSessionId}`).pipe(
+        catchError(error => {
+          onErrorReceived(error); // Call the error callback
+          return throwError(error);
         })
       );
+    }
+    return throwError('Invalid Session ID');
   }
 
-  startLoginProcess(
-    tenantName: string,
-    platform: string
-  ): Observable<string | null> {
-    const newSessionId = this.generateRandomString(15);
-    window.open(
-      `https://${platform}.login.broker/${tenantName}/auth/${platform}/session/${newSessionId}`
-    );
+  private handleStatusResponse(data: string, onErrorReceived: (error: string) => void, onSessionReceived: (sessionId: string) => void): void {
+    if (data === 'completed') {
+      onSessionReceived(this.sessionId); // Call the session callback
+    } else if (data === 'failed') {
+      console.log('Login failed. Try again');
+      onErrorReceived(data); // Call the error callback
+    } else if (data === 'pending') {
+      this.hasBeenPending = true;
+      this.retryLoginOrGiveUp(onErrorReceived, onSessionReceived);
+    } else if (this.hasBeenPending) {
+      console.log('Session expired');
+      onErrorReceived(data); // Call the error callback
+    } else {
+      console.log('Session not yet available');
+      this.retryLoginOrGiveUp(onErrorReceived, onSessionReceived);
+    }
+  }
 
-    // Wait for the window to open and save the new session in the API
-    return timer(2000).pipe(
-      switchMap(() => {
-        return this.fetchStatus(tenantName, newSessionId).pipe(
-          retryWhen((errors) =>
-            errors.pipe(
-              switchMap((error, index) => {
-                if (index < this.MAX_RETRY_COUNT) {
-                  // Retry after 2 seconds
-                  return timer(2000);
-                }
-                throw new Error('Max retries reached while pending. Giving up.');
-              }),
-              take(this.MAX_RETRY_COUNT)
-            )
-          )
-        );
-      })
-    );
+  private retryLoginOrGiveUp(onErrorReceived: (error: string) => void, onSessionReceived: (sessionId: string) => void): void {
+    if (this.retryCount < this.MAX_RETRY_COUNT) {
+      this.retryCount++;
+      timer(2000).pipe(
+        take(1),
+        tap(() => this.confirmLogin(onErrorReceived, onSessionReceived))
+      ).subscribe();
+    } else {
+      console.log('Max retries reached while pending. Giving up.');
+      onErrorReceived('Max retries reached while pending. Giving up.'); // Call the error callback
+    }
+  }
+
+  private handleError(error: any, onErrorReceived: (error: string) => void): void {
+    console.error(error);
+    onErrorReceived(error); // Call the error callback
+  }
+
+  public startLoginProcess(
+    tenantName: string,
+    platform: string,
+    onErrorReceived: (error: string) => void,
+    onSessionReceived: (sessionId: string) => void
+  ): void {
+    const newSessionId = this.generateRandomString(15);
+    window.open(`https://${platform}.login.broker/${tenantName}/auth/${platform}/session/${newSessionId}`);
+
+    // Wait for the window to open and to save the new session in the API
+    setTimeout(() => {
+      this.sessionId = newSessionId;
+      this.confirmLogin(onErrorReceived, onSessionReceived);
+    }, 2000); // Adjust the delay time as needed
+  }
+
+  public confirmLogin(onErrorReceived: (error: string) => void, onSessionReceived: (sessionId: string) => void): void {
+    if (this.sessionId) {
+      this.fetchStatus(this.sessionId, onErrorReceived, onSessionReceived).subscribe(data =>
+        this.handleStatusResponse(data, onErrorReceived, onSessionReceived)
+      );
+    }
   }
 }
